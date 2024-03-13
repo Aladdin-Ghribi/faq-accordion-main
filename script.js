1b54b723f9e851bcfa51b97b05ee9746984b8ef7
Alan <aladdinlife18@gmail.com>
2024-03-13 11:39:12 -0700

    finished

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const collapse=document.getElementById("collapse");
-let activation= document.getElementById("active");
-let active=false;
+const icons = document.querySelectorAll('.active-icon');
 
-activation.addEventListener("click",()=>{
-if(active===false){
-    activation.src="./assets/images/icon-minus.svg";
-    collapse.classList.add("show");
-    active=true
-}
-else{
-    activation.src="./assets/images/icon-plus.svg";
-    collapse.classList.remove("show");
-    active=false;
+// Loop through each icon and add event listener
+icons.forEach((icon, index) => {
+  let collapse = document.getElementById(`collapse${index + 1}`);
+  let active = false;
 
-}
-
-})
+  icon.addEventListener('click', () => {
+    if (!active) {
+      icon.src = "./assets/images/icon-minus.svg";
+      collapse.classList.add("show");
+    } else {
+      icon.src = "./assets/images/icon-plus.svg";
+      collapse.classList.remove("show");
+    }
+    active = !active;
+  });
+});
